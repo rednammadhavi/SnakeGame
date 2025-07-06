fe979783ba2dfab85b915ff379e7085b05f956fa
Rednam-madhavi <rednammadhavi@gmail.com>
2025-07-06 19:56:23 +0530

    implementation

---
+++ SnakeGame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class SnakeGame {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Snake Game");
+        GamePanel gamePanel = new GamePanel();
+
+        frame.add(gamePanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
+}
